6dbdbe2058203f5bea9935bdf0988ff721c81650
Pablo Gómez <pablogomez@pablogomez.com>
2020-04-16 13:19:17 +0100

    Stick with 2.0.7, newers are a problem

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-LatestRelease=$(curl -s https://github.com/mxpv/podsync/releases/ | grep -e 'href.*armv6' -m 1 | cut -f2 -d \")
+# LatestRelease=$(curl -s https://github.com/mxpv/podsync/releases/ | grep -e 'href.*armv6' -m 1 | cut -f2 -d \")
+LatestRelease=$(curl -s https://github.com/mxpv/podsync/releases/ | grep -e 'href.*armv6' | grep 2.0.7 | cut -f2 -d \")
 Link=https://github.com/$LatestRelease
 
 wget $Link
