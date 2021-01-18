9b9933f083e33b719102a8fd1826bdc1ea5e3f04
Pablo Gómez <pablogomez@pablogomez.com>
2021-01-18 10:08:15 +0000

    Detecting arm arch

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-LatestRelease=$(curl -s https://github.com/mxpv/podsync/releases/ | grep -e 'href.*arm64' -m 1 | cut -f2 -d \")
+if [[ ${MACHTYPE} =~ "aarch64" ]]; then
+  echo "aarch64"
+  LatestRelease=$(curl -s https://github.com/mxpv/podsync/releases/ | grep -e 'href.*arm64' -m 1 | cut -f2 -d \")
+elif [[ ${MACHTYPE} =~ "x86_64" ]]; then
+  echo "x86_64"
+  LatestRelease=$(curl -s https://github.com/mxpv/podsync/releases/ | grep -e 'href.*armv6' -m 1 | cut -f2 -d \")
+fi
 Link=https://github.com/$LatestRelease
 
 wget $Link
